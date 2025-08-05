b0239458ecf0f2e1d489813685ea0e78ba132c00
unknown <leslieclinton20@gmail.com>
2025-08-06 00:24:49 +0100

    Updates

--- components/Navbar/DesktopNav/DesktopNav.jsx
+++ components/Navbar/DesktopNav/DesktopNav.jsx
@@ -1,28 +1,54 @@
-import React from 'react';
-import styles from './styles.module.css';
+"use client";
 
+import React, { useEffect, useState } from 'react';
+import styles from './styles.module.css';
 import { NavLink } from '../NavLinks';
 import Link from 'next/link';
 
 function Navbar() {
-  return (
-    <div className={styles.nav}>
-        <div className={styles.logo}><Link href='/'> <span>
-      <span style={{ color: '#69FFAA' }}>{'{'}</span>
-      DEV.OBINNA
-      <span style={{ color: '#69FFAA' }}>{'}'}</span>
-    </span></Link></div>
+  const [scrolled, setScrolled] = useState(false);
 
-    <div className={styles.links}>
-      <ul>
-        {NavLink.map((link, i)=>(
-          <li className={ link.colored? `${styles.link} ${styles.colored}`: styles.link } key={i}> <Link href={link.url}>{link.label}{link.count? <span>{link.count}</span>: null} </Link> </li>
-        ))}
-      </ul>
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  return (
+    <div className={`${styles.nav} ${scrolled ? styles.scrolled : ''}`}>
+      <div className={styles.logo}>
+        <Link href='/'>
+          <span>
+            <span style={{ color: '#69FFAA' }}>{'{'}</span>
+            DEV.OBINNA
+            <span style={{ color: '#69FFAA' }}>{'}'}</span>
+          </span>
+        </Link>
+      </div>
+
+      <div className={styles.links}>
+        <ul>
+          {NavLink.map((link, i)=>(
+            <li 
+              className={ link.colored ? `${styles.link} ${styles.colored}` : styles.link } 
+              key={i}
+            > 
+              <Link href={link.url}>
+                {link.label}{link.count ? <span>{link.count}</span> : null} 
+              </Link> 
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-    </div>
-    
   )
 }
 
-export default Navbar
+export default Navbar;
